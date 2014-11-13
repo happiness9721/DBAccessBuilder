93da6ca3ccadbe35b2558071216999493ff5bf27
wpt2 <wpt2@wpt2teki-Mac-mini.local>
2014-11-13 13:16:25 +0800

    Initial Commit

---
+++ DataObjectBuilder/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DataObjectBuilder
+//
+//  Created by wpt2 on 11/13/14.
+//  Copyright (c) 2014 happiness9721. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
